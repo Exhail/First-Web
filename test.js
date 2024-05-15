ece0c597eaa22ff1bb18cfd806f76514489ecb1d
Elliott <Elliott_Burges@hotmail.com>
2024-05-15 17:59:03 +1000

    First commit

--- test.js
+++ test.js
@@ -1,9 +1,12 @@
+// Nav-bar Script
+
 const collapsibles = document.querySelectorAll(".collapsible");
 const shrink = document.querySelectorAll('.shrink')
 const navBtn = document.querySelectorAll(".navBtn")
 
 navBtn.forEach((item) =>
   item.addEventListener("click", function () {
+    event.preventDefault(); 
     collapsibles.forEach((collapsibles) => {
       collapsibles.classList.toggle("collapsible--hidden")
     });
@@ -12,4 +15,24 @@ navBtn.forEach((item) =>
     });
   })
 );
+
+// Projects Script
+
+const chevrons = document.querySelectorAll(".chevron")
+
+chevrons.forEach((item) => {
+  item.addEventListener("click", function () {
+    const hideableList = this.parentElement.parentElement.querySelectorAll('.hideable');
+    const morphableList = this.parentElement.parentElement.parentElement.querySelectorAll('.morphable');
+
+    hideableList.forEach((hideable) => {
+      hideable.classList.toggle("hidden")
+    });
+    morphableList.forEach((morphable) => {
+      morphable.classList.toggle('morphed')
+    });
+  })
+});
+
+
 
